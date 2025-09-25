e88c1ec765601145256ccbf3d989a60a2fdcfb35
SudoWatson <aulennert@protonmail.com>
2025-09-24 21:17:51 -0400

    Getting error in cli

--- PPA-cli.py
+++ PPA-cli.py
@@ -1,3 +1,73 @@
+import os
+import argparse
+import PPA_lib
+
+argParser = argparse.ArgumentParser(description="A python utility to help align any equotorial telescope by imaging the celestial pole region")
+
+argParser.add_argument("--solver", type=str, nargs="?", default=None, help="Whether to use online \"nova\" or \"local\" solver", required=True)
+argParser.add_argument("--horizontal", type=str, nargs="?", metavar="horizontal_file_path", default=None, help="The filepath to the horizontal image", required=True)
+argParser.add_argument("--vertical", type=str, nargs="?", metavar="vertical_file_path", default=None, help="The filepath to the vertical image", required=True)
+argParser.add_argument("--improved", type=str, nargs="?", metavar="improved_file_path", default=None, help="The filepath to the improved image after adjusting scope mount")
+
+argParser.add_argument("--cache-dir", type=str, nargs="?", default=PPA_lib.get_cache_file_path(), help="Filepath to look in for cached .wcs files")
+argParser.add_argument("--config", type=str, nargs="?", default=PPA_lib.get_config_file_path(), help="Filepath to config to use")
+argParser.add_argument("--more-data", type=bool, nargs="?", default=False, help="Returns more detailed information")
+
+args = argParser.parse_args()
+
+solver_options = ["local", "nova"]
+if args.solver not in solver_options:
+    print("Option '--solver' must be one of " + str(solver_options))
+    exit(2)
+
+"""
+Verify provided images exist or their WCS version in cache dir exists
+Verify config exists
+Load config
+Plate solve images without wcs
+Find error
+Return results
+"""
+print(args)
+
+config_file_path = args.config
+cache_dir = args.cache_dir
+return_more_data = args.more_data
+solver = args.solver
+
+hImgPath = args.horizontal
+hWcsPath = PPA_lib.get_wcs_file_path(hImgPath, cache_dir)
+if not os.path.exists(hWcsPath):
+    if not os.path.exists(hImgPath):
+        raise IOError(f"Image file '{hImgPath}' not found.")
+
+vImgPath = args.vertical
+vWcsPath = PPA_lib.get_wcs_file_path(vImgPath, cache_dir)
+if not os.path.exists(vWcsPath):
+    if not os.path.exists(vImgPath):
+        raise IOError(f"Image file '{vImgPath}' not found.")
+
+# Have the wcs files, just get the error
+print(PPA_lib.find_error(vWcsPath, hWcsPath))
+
+exit(1)
+iImgPath = args.improved
+iWcsPath = PPA_lib.get_wcs_file_path(iImgPath, cache_dir)
+if not os.path.exists(iWcsPath):
+    if not os.path.exists(iImgPath):
+        raise IOError(f"Image file '{iImgPath}' not found.")
+
+
+
+
+
+
+
+
+
+
+
+
 # ppa-cli --horizontal /path/to/horiz/image --verticle /path/horiz/image --[local|nova]
 # Returns > left: 6.156705  down: 21.456830
 
@@ -46,34 +116,3 @@
     ]
 }
 """
-
-import argparse
-import PPA_lib
-
-argParser = argparse.ArgumentParser(description="A python utility to help align any equotorial telescope by imaging the celestial pole region")
-
-argParser.add_argument("--solver", type=str, nargs="?", default=None, help="Whether to use online \"nova\" or \"local\" solver", required=True)
-argParser.add_argument("--horizontal", type=str, nargs="?", metavar="horizontal_file_path", default=None, help="The filepath to the horizontal image", required=True)
-argParser.add_argument("--vertical", type=str, nargs="?", metavar="vertical_file_path", default=None, help="The filepath to the vertical image", required=True)
-argParser.add_argument("--improved", type=str, nargs="?", metavar="improved_file_path", default=None, help="The filepath to the improved image after adjusting scope mount")
-
-argParser.add_argument("--cache-dir", type=str, nargs="?", default=PPA_lib.get_cache_file_path(), help="Filepath to look in for cached .wcs files")
-argParser.add_argument("--config", type=str, nargs="?", default=PPA_lib.get_config_file_path(), help="Filepath to config to use")
-argParser.add_argument("--more-data", type=bool, nargs="?", default=False, help="Returns more detailed information")
-
-args = argParser.parse_args()
-
-solver_options = ["local", "nova"]
-if args.solver not in solver_options:
-    print("Option '--solver' must be one of " + str(solver_options))
-    exit(2)
-
-"""
-Verify provided images exist or their WCS version in cache dir exists
-Verify config exists
-Load config
-Plate solve images without wcs
-Find error
-Return results
-"""
-
